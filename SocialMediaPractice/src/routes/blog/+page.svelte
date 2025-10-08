c680edb5508bdb23c2cc758becc09246cc6c7ae4
wheerh1 <wheerh1@student.op.ac.nz>
2025-10-08 16:05:48 +1300

    style tidying

--- SocialMediaPractice/src/routes/blog/+page.svelte
+++ SocialMediaPractice/src/routes/blog/+page.svelte
@@ -1,18 +1,24 @@
 <script>
+    let { data } = $props();
 </script>
 
 <h2>Myblog</h2>
 
 <nav>
-	<a href="/blog/whatever">whatever</a>
-    <a href="/blog/seems">seems</a>
-    <a href="/blog/cool">cool</a>
-    <a href="/blog/Hiya">hiya</a>
+	
+    {#each data.posts as post}
+        <a href="/blog/{post.title}">{post.title}</a>
+    {/each}
 </nav>
 
 <style>
     nav{
         display: flex;
         flex-direction: column;
+        
+    }
+    a{
+        border-radius: 10px;
+        padding: 3px;
     }
 </style>
