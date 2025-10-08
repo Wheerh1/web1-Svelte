ddfce005b9c51c38343d06877aa90cb0aa8a31d6
wheerh1 <wheerh1@student.op.ac.nz>
2025-10-08 15:51:13 +1300

    blog dynamic routing

---
+++ SocialMediaPractice/src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,3 @@
+<h2>blog title</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas doloribus consectetur hic dolorum velit minus quos incidunt exercitationem aspernatur eveniet corrupti, error nostrum harum, laborum, numquam qui animi reprehenderit sed.</p>
