9e0786a4639340d97849475f096bc04adca0ad7f
wheerh1 <wheerh1@student.op.ac.nz>
2025-10-28 10:48:14 +1300

    table exists

--- responsiveness/responsiveness/src/routes/recipies/+page.svelte
+++ responsiveness/responsiveness/src/routes/recipies/+page.svelte
@@ -1,5 +1,31 @@
 <script>
     let {data} = $props();
-    console.log(data)
+    console.log(data.recipes)
 </script>
 
+<table>
+    <thead>
+        <tr>
+            <td>Rescipie name</td>
+            <td>prep time</td>
+            <td>difficulty</td>
+            <td>servings</td>
+            <td>cusine</td>
+        </tr>
+    </thead>
+    <tbody>
+{#each data.recipes.recipes as recipe}
+        <tr>
+            <td>{recipe.name}</td>
+            <td>{recipe.prepTimeMinutes}</td>
+            <td>{recipe.difficulty}</td>
+            <td>{recipe.servings}</td>
+            <td>{recipe.cusine}</td>
+        </tr>
+    {/each}
+    </tbody>
+</table>
+
+<style>
+    
+</style>
