fe1a06a2f706fcf8f28c6bf48b52e5f94cf024b0
wheerh1 <wheerh1@student.op.ac.nz>
2025-10-28 10:33:19 +1300

    updating to correct api link

--- responsiveness/responsiveness/src/routes/recipies/+page.js
+++ responsiveness/responsiveness/src/routes/recipies/+page.js
@@ -1,5 +1,5 @@
 export async function load({fetch}) {
-    const res = await fetch ("https://my.api.mockaroo.com/players.json?key=6e8297b0");
-    const players = await res.json();
-    return {players};
+    const res = await fetch ("'https://dummyjson.com/recipes/1'");
+    const recipes = await res.json();
+    return {recipes};
 }
