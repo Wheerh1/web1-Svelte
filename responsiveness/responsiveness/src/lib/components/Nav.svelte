2c09d8ad6e760b20fd7c7fa0ff53bea6f5b40590
wheerh1 <wheerh1@student.op.ac.nz>
2025-10-28 11:07:51 +1300

    adding basic styling

--- responsiveness/responsiveness/src/lib/components/Nav.svelte
+++ responsiveness/responsiveness/src/lib/components/Nav.svelte
@@ -1,6 +1,7 @@
 <nav>
     <ul>
         <li><a href="/">Home</a></li>
+        <li><a href="/about">about</a></li>
         <li><a href="/recipies">recipes</a></li>
         <li><a href="/contact">contact us</a></li>
     </ul>
@@ -10,6 +11,12 @@
     ul{
         list-style: none;
         display:flex;
-        
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    li{
+        padding: 1em;
+        background-color: aliceblue;
+        border-radius: 2em;
     }
 </style>
